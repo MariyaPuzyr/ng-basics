8ad0e2125a92a22d4aba249c57e784e779e0cd2b
MariyaPuzyr <mariyapyzur@gmail.com>
2020-03-03 11:54:24 +0200

    24+25 lesson

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,18 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
   template: `
-    <input type="text" [(ngModel)]="name">
+    <!--<input type="text" [(ngModel)]="name">-->
+    <input type="text" (input)="onUserInput($event)" [value]="name">
     <h4>Hello {{name}}!</h4>
     <p>User Component</p>
   `
 })
 
 export class UserComponent {
-  name = 'Mariya';
+  @Input() name;
+  @Output() nameChanged = new EventEmitter<string>();
 
   onUserInput(event) {
-    this.name = event.target.value;
+    // this.name = event.target.value;
+      this.nameChanged.emit(event.target.value);
   }
 }
