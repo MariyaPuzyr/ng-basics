519090359619c732b158ee5f83e8564c37d61d6f
MariyaPuzyr <mariyapyzur@gmail.com>
2020-03-03 16:46:38 +0200

    35 lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  number = 0;
   title = 'ng-basics';
   rootName = 'testName';
   rootItems = ['Apples', 'Bananas', 'Cherries'];
@@ -14,5 +15,8 @@ export class AppComponent {
   onItemWasAdded(event) {
     this.rootItems.push(event);
     console.log(this.rootItems);
+  }
+  onIncrease() {
+    this.number = this.number * 2;
   }
 }
